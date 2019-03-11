482d784224e3a1c1ccff532a4c2c9a23e1ecc133
Lee kwangsung <igwangseong@LEEui-MacBook-Pro.local>
2019-03-11 22:36:19 +0900

    allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,11 @@ from .base import env
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['gangram.com'])
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+            "*",
+        "localhost",
+        "http://kwanggram.ap-northeast-2.elasticbeanstalk.com/",
+    ]
 # DATABASES
 # ------------------------------------------------------------------------------
 # DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
